e751681d1b4fa4b1b1af54b825116942653c5b3e
Edson Ninomiya <kenjinino07@gmail.com>
2013-04-26 16:52:15 -0300

    Commented some unneeded automatically generated tests

--- spec/helpers/todolists_helper_spec.rb
+++ spec/helpers/todolists_helper_spec.rb
@@ -10,6 +10,6 @@ require 'spec_helper'
 #     end
 #   end
 # end
-describe TodolistsHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+#describe TodolistsHelper do
+#  pending "add some examples to (or delete) #{__FILE__}"
+#end
